d3e287a274af9db0cab9ba8b8972d907b3b3a6ac
Kevin D'souza <kevin.d@logiq.ai>
2023-03-24 16:51:53 +0530

    feat: add command to delete dashboards
    Signed-off-by: Kevin D'souza <kevin.d@logiq.ai>

---
+++ cmd/delete.go
@@ -0,0 +1,49 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/logiqai/logiqctl/ui"
+	"github.com/logiqai/logiqctl/utils"
+	"github.com/spf13/cobra"
+)
+
+var deleteCmd = &cobra.Command{
+	Use:   "delete <resource_name>",
+	Short: "Delete one or more of your LOGIQ resources",
+	Example: `
+Delete dashboards
+logiqctl delete dashboard slug		
+	`,
+}
+
+func init() {
+	rootCmd.AddCommand(deleteCmd)
+	deleteCmd.AddCommand(deleteDashboardsCommand())
+}
+
+func deleteDashboardsCommand() *cobra.Command {
+	return &cobra.Command{
+		Use:     "dashboard",
+		Example: "logiqctl delete dashboard slug",
+		Aliases: []string{"dashboard"},
+		Short:   "Delete LOGIQ Dashboards",
+		PreRun:  utils.PreRunUiTokenOrCredentials,
+		Run: func(cmd *cobra.Command, args []string) {
+			if len(os.Args) < 4 {
+				fmt.Println("Slug not provided")
+				os.Exit(1)
+			}
+
+			slug := os.Args[3]
+
+			if err := ui.DeleteDashboard(slug); err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+
+			fmt.Printf("Successfully deleted dashboard: %s\n", slug)
+		},
+	}
+}
